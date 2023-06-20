1dcd4d10e909db6b32409bf7e822a25c0d718277
nleake <6044084+nleake@users.noreply.github.com>
2023-06-20 15:54:38 -0400

    initial commit, complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes = 1) {
+    let newString = '';
+    if (string < 0 || numTimes < 0) { return 'ERROR'}
+    for (let index = 0; index < numTimes; index++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
