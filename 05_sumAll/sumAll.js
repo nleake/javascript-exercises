7d87a24695f762e30982fa2500754f8859863e44
nleake <6044084+nleake@users.noreply.github.com>
2023-06-21 16:22:38 -0400

    complete exercises 3 and 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) { return 'ERROR' };
+    if (start < 0 || end < 0) { return 'ERROR' };
+    if (start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
 
+    let sum = start;
+    while (start < end) {
+        start++;
+        sum += start;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
