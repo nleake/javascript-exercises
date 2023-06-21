dc2cac6ab6312e4e1a1f8431ad55cc9c9e283303
nleake <6044084+nleake@users.noreply.github.com>
2023-06-21 16:23:13 -0400

    update exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...items) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (items.indexOf(element) === -1
+        && typeof element === typeof arr[i]) { 
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
+
+removeFromArray([1, 2, 3], 2, 3, 4, 5, 6);
 
 // Do not edit below this line
 module.exports = removeFromArray;
