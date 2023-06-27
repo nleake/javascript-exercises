1f754f101d95ba67d69c91f366529bb25d312061
nleake <6044084+nleake@users.noreply.github.com>
2023-06-27 13:26:54 -0400

    add palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    //get rid of non-alphanumeric chars
+  const re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+  let lowRegStr = str.toLowerCase().replace(re, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');    
+  return reverseStr === lowRegStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
