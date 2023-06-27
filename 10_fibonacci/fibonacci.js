73b0c2a99bf8e3c97945fdf1cc4588d3f386ed22
nleake <6044084+nleake@users.noreply.github.com>
2023-06-27 14:36:11 -0400

    complete 10, 11, 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(item) {
+    item = Number.parseInt(item);
+    if (item < 0 ) return "OOPS";
+    const fibNumbers = [1, 1, 2, 3, 5, 8];
+    if (item > fibNumbers.length) {
+        while (item > fibNumbers.length) {
+            let next = fibNumbers[fibNumbers.length - 1]
+            let prior = fibNumbers[fibNumbers.length - 2]
+            let newFib = next + prior;
+            fibNumbers.push(newFib)
+        }
+    }
+    return fibNumbers[item-1];
 };
-
+    
 // Do not edit below this line
 module.exports = fibonacci;
